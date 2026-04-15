24f781bec929d73d3a62ac9466f2ec3af8710579
Muhammed Ahad <maaahad@gmail.com>
2026-04-15 22:40:16 +0200

    SockerAddr from std to create socket_adddr

--- apps/api/rust_graphql/src/main.rs
+++ apps/api/rust_graphql/src/main.rs
@@ -1,9 +1,11 @@
 use axum::{Router, routing::get};
+use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/", get(|| async { "Hello, World!!" }));
+    let socket_addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind(socket_addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
